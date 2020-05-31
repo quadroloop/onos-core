08fd9f868bddd4eaec2505c92f3d3cfdbc5c1b23
Neil Monzales <neil.monzales@whitecloak.com>
2020-05-31 12:10:48 +0800

    group items by last updated date

--- src/routes/forecasts.route.ts
+++ src/routes/forecasts.route.ts
@@ -2,11 +2,33 @@ const express = require("express");
 const got = require("got");
 const router = express.Router();
 
+interface IData {
+  last_update: string;
+  source: string;
+  location: string;
+}
+
+const groupBy = (arr, fn) =>
+  arr
+    .map(typeof fn === "function" ? fn : (val) => val[fn])
+    .reduce((acc, val, i) => {
+      acc[val] = (acc[val] || []).concat(arr[i]);
+      return acc;
+    }, {});
 /* GET home page. */
 router.get("/", async function (req, res, next) {
   const result = await got("http://noah.up.edu.ph/api/four_hour_forecast");
-  const data = JSON.parse(result.body);
-  res.json({ data });
+  const body = JSON.parse(result.body);
+  const data = Object.entries(groupBy(body, "last_update")).map(
+    ([key, value]) => {
+      const val = value as [];
+      return {
+        last_update: key,
+        data: val.map((item: {}) => ({ ...item, last_update: undefined })),
+      };
+    }
+  );
+  res.json(data);
 });
 
 module.exports = router;
